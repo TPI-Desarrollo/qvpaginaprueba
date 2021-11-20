4f963ca266fdafd45e7dd8576968c06d4b87a92a
ssuarezs <suarez.santiago.113@gmail.com>
2021-11-20 16:27:55 -0500

    fix calendar

--- calendario/calendario.js
+++ calendario/calendario.js
@@ -12,6 +12,12 @@ $(document).ready(function(){
             {startDate: "2021-10-04T19:00:00.000Z", endDate: "2021-10-04T24:00:00.000Z", summary: "Inicio - Primer Piso"},
             {startDate: "2021-10-20T24:00:00.000Z", endDate: "2021-10-20T24:00:00.000Z", summary: "Entrega - Primer Piso"},
             {startDate: "2021-10-25T24:00:00.000Z", endDate: "2021-10-25T24:00:00.000Z", summary: "Inicio - Segundo Piso"},
+            {startDate: "2021-11-08T24:00:00.000Z", endDate: "2021-11-08T24:00:00.000Z", summary: "Mentoria"},
+            {startDate: "2021-11-10T24:00:00.000Z", endDate: "2021-11-10T24:00:00.000Z", summary: "Mentoria formativa (entrega de piso)"},
+            {startDate: "2021-11-10T24:00:00.000Z", endDate: "2021-11-10T24:00:00.000Z", summary: "Plazo máximo de enviar preguntas para conversatorio"},
+            {startDate: "2021-11-17T24:00:00.000Z", endDate: "2021-11-17T24:00:00.000Z", summary: "Conversatorio (Habilidades blandas, Tecnología y sociedad, Economía del conocimiento)"},
+            {startDate: "2021-11-22T24:00:00.000Z", endDate: "2021-11-22T24:00:00.000Z", summary: "Charla de Género"},
+            {startDate: "2021-11-24T24:00:00.000Z", endDate: "2021-11-24T24:00:00.000Z", summary: "Mentoria"},
         ],                     // List of events
         onInit: function (calendar) {}, // Callback after first initialization
         onMonthChange: function (month, year) {}, // Callback on month change
